b45a5a8e17ebc83beec357ad8d80a3ad83c8142c
wallacesintra <wallvxwallace962@gmail.com>
2024-03-09 23:34:30 +0300

    optimized imports

--- app/src/main/java/com/example/contacts/ContactScreen.kt
+++ app/src/main/java/com/example/contacts/ContactScreen.kt
@@ -1,8 +1,6 @@
 package com.example.contacts
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,35 +12,34 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.Button
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun ContactScreen(
     state: ContactState,
     onEvent: (ContactEvent) -> Unit
+
 ){
     Scaffold (
         floatingActionButton = {
             FloatingActionButton(onClick = { onEvent(ContactEvent.ShowDialog)}) {
                 Icon(imageVector = Icons.Default.Add, contentDescription = "Add Contact")
             }
+        },
+        topBar = {
+            Text(
+                text = "Contacts",
+                style = MaterialTheme.typography.displayMedium
+            )
         }
     ){ padding->
         if (state.isAddingContact){
@@ -66,7 +63,7 @@ fun ContactItem(
     onEvent: (ContactEvent) -> Unit
 ){
     Row(
-        modifier = Modifier
+        modifier = modifier
             .padding(10.dp)
             .fillMaxWidth()
     ) {
